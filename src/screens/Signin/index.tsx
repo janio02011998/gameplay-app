37bdbf38197d0c1fdbf01ccc99be7db36706db4b
Janio Carvalho <carvalho02011998@gmail.com>
2021-08-13 17:01:03 -0300

    Add + folders

---
+++ src/screens/Signin/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { styles } from './style';
+export function SignIn() {
+    return (
+        <View style={styles.container}>
+            <Text>Hello Wolrd!</Text>
+        </View>
+    );
+}
